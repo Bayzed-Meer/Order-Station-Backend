900fc0e8fa53324e1fa5b6ba73ccd688284b9e21
MD Bayzed Meer <bayzed.meer@brainstation-23.com>
2024-06-12 15:24:47 +0600

    feat: add employee and staff models

---
+++ src/models/employee.model.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
+const validator = require("validator");
+
+const employeeSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function (value) {
+        return validator.isEmail(value);
+      },
+      message: (props) => `${props.value} is not a valid email address!`,
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (value) {
+        return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
+          value
+        );
+      },
+      message: (props) =>
+        `${props.value} is not a valid password! Password must be at least 8 characters containing at least one letter, one digit, one special character, and one capital letter`,
+    },
+  },
+  role: {
+    type: String,
+    default: "employee",
+  },
+});
+
+// Hash the password before saving it to the database
+employeeSchema.pre("save", async function (next) {
+  const employee = this;
+  if (!employee.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(employee.password, salt);
+    employee.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+const Employee = mongoose.model("Employee", employeeSchema);
+
+module.exports = Employee;
---
+++ src/models/staff.model.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
+const validator = require("validator");
+
+const staffSchema = new Schema({
+  id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function (value) {
+        return validator.isEmail(value);
+      },
+      message: (props) => `${props.value} is not a valid email address!`,
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (value) {
+        return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
+          value
+        );
+      },
+      message: (props) =>
+        `${props.value} is not a valid password! Password must be at least 8 characters containing at least one letter, one digit, one special character, and one capital letter`,
+    },
+  },
+  role: {
+    type: String,
+    default: "staff",
+  },
+});
+
+// Hash the password before saving it to the database
+staffSchema.pre("save", async function (next) {
+  const staff = this;
+  if (!staff.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(staff.password, salt);
+    staff.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+const Staff = mongoose.model("Staff", staffSchema);
+
+module.exports = Staff;
